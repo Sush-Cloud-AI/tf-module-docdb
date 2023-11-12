e09825ed7524e811ad1d1a280237f3770c5939dd
Sush <sushcloud12@gmail.com>
2023-11-12 18:15:48 +0000

    i

--- schema.tf
+++ schema.tf
@@ -1,39 +1,39 @@
-# # # Injecting the schema
-resource "null_resource" "schema" {
+# # # # Injecting the schema
+# resource "null_resource" "schema" {
 
-depends_on = [aws_docdb_cluster_instance.cluster_instances, aws_docdb_cluster.docdb]  
-provisioner "local-exec" {
-        command =  "curl -s -L -o mongodb.zip 'https://github.com/stans-robot-project/mongodb/archive/main.zip'"
-            
-        }
-
-provisioner "local-exec" {
-        command = "unzip -o /var/lib/jenkins/workspace/terraform-databases/mongodb.zip"
-            
-        }
+# depends_on = [aws_docdb_cluster_instance.cluster_instances, aws_docdb_cluster.docdb]  
 # provisioner "local-exec" {
-#         command = "cd /var/lib/jenkins/workspace/terraform-databases/mongodb-main/"
+#         command =  "curl -s -L -o mongodb.zip 'https://github.com/stans-robot-project/mongodb/archive/main.zip'"
             
 #         }
-provisioner "local-exec" {
-        command = "mv mongodb-main/catalogue.js /tmp"
-            
-        }
-provisioner "local-exec" {
-        command = "mv mongodb-main/users.js /tmp"
-            
-        }
-provisioner "local-exec" {
-        command =  "wget -nc -P /tmp/ https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem"
-            
-        }
-provisioner "local-exec" {
-        command = "mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile tmp/global-bundle.pem --username admin1 --password roboshop1 < catalogue.js"
-            
-        }
- provisioner "local-exec" {
-        command = "mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile tmp/global-bundle.pem --username admin1 --password roboshop1 < users.js"
-            
-        }      
 
-    }
+# provisioner "local-exec" {
+#         command = "unzip -o /var/lib/jenkins/workspace/terraform-databases/mongodb.zip"
+            
+#         }
+# # provisioner "local-exec" {
+# #         command = "cd /var/lib/jenkins/workspace/terraform-databases/mongodb-main/"
+            
+# #         }
+# provisioner "local-exec" {
+#         command = "mv mongodb-main/catalogue.js /tmp"
+            
+#         }
+# provisioner "local-exec" {
+#         command = "mv mongodb-main/users.js /tmp"
+            
+#         }
+# provisioner "local-exec" {
+#         command =  "wget -nc -P /tmp/ https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem"
+            
+#         }
+# provisioner "local-exec" {
+#         command = "mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile tmp/global-bundle.pem --username admin1 --password roboshop1 < catalogue.js"
+            
+#         }
+#  provisioner "local-exec" {
+#         command = "mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile tmp/global-bundle.pem --username admin1 --password roboshop1 < users.js"
+            
+#         }      
+
+#     }
