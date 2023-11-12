452316807d9a0772a4c41dc22cfbe99665bcd88a
Sush <sushcloud12@gmail.com>
2023-11-12 17:22:30 +0000

    i

---
+++ schema.tf
@@ -0,0 +1,35 @@
+# # # # Injecting the schema
+resource "null_resource" "schema" {
+
+depends_on = [aws_docdb_cluster_instance.cluster_instances, aws_docdb_cluster.docdb]  
+provisioner "local-exec" {
+        command =  "curl -s -L -o mongodb.zip 'https://github.com/stans-robot-project/mongodb/archive/main.zip'"
+            
+        }
+
+provisioner "local-exec" {
+        command = "unzip -o /var/lib/jenkins/workspace/terraform-databases/mongodb.zip"
+            
+        }
+# provisioner "local-exec" {
+#         command = "cd /var/lib/jenkins/workspace/terraform-databases/mongodb-main/"
+            
+#         }
+provisioner "local-exec" {
+        command = "mv mongodb-main/catalogue.js /tmp"
+            
+        }
+provisioner "local-exec" {
+        command = "mv mongodb-main/users.js /tmp"
+            
+        }
+# provisioner "local-exec" {
+#         command = " mysql -h ${aws_db_instance.mysql.address} -uadmin1 -proboshop1 < /tmp/catalogue.js"
+            
+#         }
+#  provisioner "local-exec" {
+#         command = "rm -rf /tmp/shipping.sql"
+            
+#         }      
+
+    }
